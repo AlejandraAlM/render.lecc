01063f7bf4b05b346d83406de1b1ccf32274be96
AlejandraAlM <166570941+AlejandraAlM@users.noreply.github.com>
2024-04-28 09:39:29 -0600

    add info readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# render.lecc
+# Venta de autos
+
+# Repositorio
+
+#URL
+
+# Aplicación web
+
+# url
+
+# La app es conformada por:
+# Encabezado
+# Check box que permite desplegar el histograma
+# Casilla de verificación para mostrar el gráfico de dispersión.
--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+st.header('Histograma y gráfica de dispersión') #Encabezado
+
 hist_button = st.button('Construir histograma') # crear un botón
 
 if hist_button: # al hacer clic en el botón
@@ -13,5 +17,16 @@ if hist_button: # al hacer clic en el botón
     fig = px.histogram(car_data, x="odometer")
 
     # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+build_disp = st.checkbox('Construir un gráfico de dispersión')
+
+if build_disp: # si la casilla de verificación está seleccionada
+    st.write('Construir un gráfico de dispersión para la columna odómetro')
+    
+    #crear gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+    
+    #mostrar gráfico
     st.plotly_chart(fig, use_container_width=True)
 
